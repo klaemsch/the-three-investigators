fc55d1ccd1c7e07cd18b957bfb8cae0b5b3837c9
Klemens Wohlfarth <klemens.wohlfarth@check24.de>
2026-01-31 17:44:53 +0100

    added scripts 008 and 045

---
+++ extract/README.md
@@ -0,0 +1,9 @@
+# Fehlerliste
+
+Teilweise werden die Wörter mit Bindestrichen gebrochen. In einer Zeile steht dann am Ende "in seiner Woh-" und in der nächsten "nung. Und ". Wenn wir die Zeilen zusammen fügen müssen wir die Bindestriche am Ende der Wörter entfernen.
+
+003
+- "1 Im Buch: Mrs. Boggle"
+
+008
+- PolizistWird, PolizistVorsicht
--- extract/settings.py
+++ extract/settings.py
@@ -22,10 +22,33 @@ page_settings = {
         "speaker_name_window": [7, 23],
         "text_start_index": 20,
         "speaker_name_window_threshold": 0.5
+    },
+    "008": {
+        "id": "008",
+        "speaker_name_window": [10, 22],
+        "text_start_index": 20,
+        "speaker_name_window_threshold": 0.5
+    },
+    "045": {
+        "id": "045",
+        "speaker_name_window": [6, 21],
+        "text_start_index": 20,
+        "speaker_name_window_threshold": 0.5
     }
 }
 
 # if any of these strings appear in a line, the line is ignored
 ignore_line_markers = ['www.rocky-beach.com', 'www. rocky-beach.com']
 
-additional_speakers = ['Justus', 'Bob', 'Peter', 'Elmquist', 'Murphy', 'Mr Prentice', 'Mrs Boogle', 'Ms Chalmers', 'Hassell', 'Peter, Bob', 'Justus, Peter', 'Tommy', 'Justua', 'Schwarzbart', 'Allgemein', 'Mrs.Peterson', 'DwiggIns', '3???/ Gus', '3???', 'Gus', 'Rhandur', 'Dwiggins', 'Hitchcock', 'Mathilda', 'Allen', 'Shelby', 'Die ???', 'Reynolds', 'Carson']
+additional_speakers = [
+    'Justus', 'Bob', 'Peter', 'Elmquist', 'Murphy', 
+    'Mr Prentice', 'Mrs Boogle', 'Ms Chalmers', 'Hassell', 
+    'Peter, Bob', 'Justus, Peter', 'Tommy', 'Justua', 
+    'Schwarzbart', 'Allgemein', 'Mrs.Peterson', 'DwiggIns', 
+    '3???/ Gus', '3???', 'Gus', 'Rhandur', 'Dwiggins', 
+    'Hitchcock', 'Mathilda', 'Allen', 'Shelby', 'Die ???', 
+    'Reynolds', 'Carson', '1.Mann', '2.Mann', '3.Mann', 
+    'Carlson', 'Calson', 'Green', 'Tonband', 'Just', 'Stimme',
+    'Wikinger', 'Indianer', 'Radiostimme', 'Kapitän/Sam', 
+    'Karl', 'Bob/Peter', 'Coulter/Sam', 'Ingmar', 'Manning'
+]
