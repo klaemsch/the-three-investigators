dfee2f23349b4e6f786f4be2de625344a0ded485
Klemens Wohlfarth <klemens.wohlfarth@check24.de>
2026-02-01 19:47:24 +0100

    030, remove text start index

--- extract/settings.py
+++ extract/settings.py
@@ -1,68 +1,59 @@
 page_settings = {
     "003": {
         "id": "003",
-        "text_start_index": 20,
         "speaker_name_window_threshold": 0.2
     },
     "004": {
         "id": "004",
-        "text_start_index": 20,
         "speaker_name_window_threshold": 0.5
     },
     "005": {
         "id": "005",
-        "text_start_index": 20,
         "speaker_name_window_threshold": 0.5
     },
     "007": {
         "id": "007",
-        "text_start_index": 20,
         "speaker_name_window_threshold": 0.5
     },
     "008": {
         "id": "008",
-        "text_start_index": 20,
+        "speaker_name_window_threshold": 0.5
+    },
+    "030": {
+        "id": "030",
         "speaker_name_window_threshold": 0.5
     },
     "041": {
         "id": "041",
-        "text_start_index": 24,
         "speaker_name_window_threshold": 0.5
     },
     "045": {
         "id": "045",
-        "text_start_index": 20,
         "speaker_name_window_threshold": 0.5
     },
     "074": {
         "id": "074",
-        "text_start_index": 22,
         "speaker_name_window_threshold": 0.1,
         "regie_in_parentheses": True
     },
     "080": {
         "id": "080",
-        "text_start_index": 22,
         "speaker_name_window_threshold": 0.2
     },
     "101": {
         "id": "101",
-        "text_start_index": 19,
         "speaker_name_window_threshold": 0.2
     },
     "107": {
         "id": "107",
-        "text_start_index": 22,
         "speaker_name_window_threshold": 0.2
     },
     "130": {
         "id": "130",
-        "text_start_index": 24,
         "speaker_name_window_threshold": 0.2
     },
     "190": {
         "id": "190",
-        "text_start_index": 25,
         "speaker_name_window_threshold": 0.2
     }
 }
@@ -77,7 +68,8 @@ italic_font_names = [
 ignore_line_markers = [
     'www.rocky-beach.com', 
     'www. rocky-beach.com',
-    'Die drei ??? – ...und die Kammer der Rätsel (190)'
+    'Die drei ??? – ...und die Kammer der Rätsel (190)',
+    'www.rocky-beach. com',
 ]
 
 additional_speakers = [
@@ -97,5 +89,7 @@ additional_speakers = [
     'Mrs Jones', 'Mr Cade', 'Mr. Brewster', '1. Polizist',
     'Beide Polizisten', 'Titus', 'Robinshaw', 'Peter.',
     'Cotta', 'Mrs. Hanson', 'Whitehead', 'Wright', 
-    'John+Jeffrey', 'White', 'Die ganze Sekte'
+    'John+Jeffrey', 'White', 'Die ganze Sekte', 'Mr. Crowe',
+    'Mr. MacGruder', 'Mr. Hanley', 'Connors', 'Demonstrant 1',
+    'Demonstrant 2'
 ]
