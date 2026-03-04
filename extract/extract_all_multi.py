d16c255347ae2316df888f9b7ac468b979f53a45
klaemsch <54587753+klaemsch@users.noreply.github.com>
2026-03-05 00:13:54 +0100

    format

--- extract/extract_all_multi.py
+++ extract/extract_all_multi.py
@@ -6,21 +6,22 @@ import logging
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 
+
 def notebook_worker(id_str: str):
-    logging.info(f'Starting notebook for script {id_str}')
+    logging.info(f"Starting notebook for script {id_str}")
     try:
         pm.execute_notebook(
             "template.ipynb",
             f"output/output_{id_str}.ipynb",
-            parameters={"script_number": id_str}
+            parameters={"script_number": id_str},
         )
     except pm.exceptions.PapermillExecutionError as e:
-        if e.ename == 'FileNotFoundError':
-            print(f'pdf file for id {id} could not be found')
+        if e.ename == "FileNotFoundError":
+            print(f"pdf file for id {id} could not be found")
         else:
             logging.error(f"Exception in notebook with id {id_str}: {e}")
 
 
-id_strings = [f'{id:>03}' for id in range(0, 226)]
+id_strings = [f"{id:>03}" for id in range(0, 226)]
 with concurrent.futures.ThreadPoolExecutor() as executor:
     executor.map(notebook_worker, id_strings)
